185f636e1514613435e8ccd140dc3ac2f8210dab
Sanchit Mehta <sanchit.mehta602@gmail.com>
2019-12-06 10:36:34 -0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 echo "....."
 ls
 echo "....."
-curl -X POST --data-binary @foo.zip http://test4:debug@test4.scm.antaresbuildsvc.internaltestkube.com/api/zipdeploy
+curl -X POST --data-binary @foo.zip http://testapp123:debug@testapp123.scm.antaresbuildsvc.internaltestkube.com/api/zipdeploy
